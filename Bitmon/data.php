0d6b5c3d91dc439c5d8fe0e9df74e4820a015a74
Ikuzot <110021129+Ikuzot@users.noreply.github.com>
2023-01-06 14:09:36 +0700

    Add files via upload

---
+++ Bitmon/data.php
@@ -0,0 +1,4 @@
+<?php
+$ipp = 'http://Remunan88:Remunan88@185.199.229.156:7492';
+$cok = "_ga=GA1.1.21416694.1672985658; bitmedia_fid=eyJmaWQiOiJiMzI5ZWRmODRlYTlkOWJlODQyYzIxZjhmYWVmNzA5OSIsImZpZG5vdWEiOiI1ODhjNDc1YWMzNmFhNzY0MmFmZGVhNjcwMGM1OTdkNCJ9; SesHashKey=249a8uxlzo52qxpg; SesToken=ses_id=442023&ses_key=249a8uxlzo52qxpg; AccExist=442023; cf_chl_rc_m=3; PHPSESSID=07buqvio47kg6kjf5drdlkljc8; _ga_7Z81E54NN3=GS1.1.1672985658.1.1.1672988685.0.0.0";
+$us = "Mozilla/5.0 (Linux; Android 8.1.0; C12 Pro Build/O11019) AppleWebKit/537.36 (KHTML, like Gecko) Kiwi Chrome/69.0.3440.40 Mobile Safari/537.36";
