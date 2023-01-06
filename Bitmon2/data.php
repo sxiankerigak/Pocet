585dba59e9b020d1ce76fa65dac2a1adb0483695
Ikuzot <110021129+Ikuzot@users.noreply.github.com>
2023-01-06 16:43:33 +0700

    Create data.php

---
+++ Bitmon2/data.php
@@ -0,0 +1,4 @@
+<?php
+$ipp = 'http://Remunan88:Remunan88@185.199.228.220:7300';
+$cok = "_ga=GA1.1.1701280950.1672825367; bitmedia_fid=eyJmaWQiOiJiMzI5ZWRmODRlYTlkOWJlODQyYzIxZjhmYWVmNzA5OSIsImZpZG5vdWEiOiI1ODhjNDc1YWMzNmFhNzY0MmFmZGVhNjcwMGM1OTdkNCJ9; SesHashKey=q6803gf2zopu0gfw; SesToken=ses_id=440585&ses_key=q6803gf2zopu0gfw; AccExist=440585; cf_clearance=pynLWlr5Kbd1vUq9mRZuPoTWInnuHkXrOfp5tmRzruQ-1672902488-0-160; PHPSESSID=ojbricmc2ip4iaoucl6tgr99cn; cf_chl_rc_m=1; _ga_7Z81E54NN3=GS1.1.1672905339.4.1.1672905754.0.0.0";
+$us = "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36";
