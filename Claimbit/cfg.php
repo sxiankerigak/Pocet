ec09edf14a97131edc2b3bdc2c4f0d7d34b215bf
Ikuzot <110021129+Ikuzot@users.noreply.github.com>
2023-01-07 11:41:10 +0700

    Add files via upload

---
+++ Claimbit/cfg.php
@@ -0,0 +1,8 @@
+<?php
+$proxy_ip = '185.199.231.45';
+$proxy_port = '8382';
+$Username = 'Remunan88';
+$Password = 'Remunan88';
+$cok = "_gid=GA1.2.721079494.1672892196; clever-last-tracker-53577=1; bitmedia_fid=eyJmaWQiOiJiZmE3YjY2M2U2YzQzMThkMzViMTcyMjAyZTZiOWFkZCIsImZpZG5vdWEiOiJjODE1ZGNiMDhjZDBmYmViYmUwMDIxMTkxZTkyNGU4OCJ9; AccExist=242183; SesHashKey=zsusoymuwwxqfbu6; SesToken=ses_id=242183&ses_key=zsusoymuwwxqfbu6; uuid=P55d93bb2-ef23-4fc3-87b2-930c34b483d2; PHPSESSID=elmfm258jel26dcmh1l7lhue58; cf_clearance=Qx3e9S0zCc8PEX9tkC5bzL9.OHBSAMqficxIql2.6PY-1673066158-0-160; _ga_J2L7YD89W2=GS1.1.1673065937.21.1.1673066170.0.0.0; _ga=GA1.2.2063216898.1672892192";
+$us = "Mozilla/5.0 (Linux; Android 13; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36";
+
