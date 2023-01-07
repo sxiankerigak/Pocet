e24f87ce979ca69fa2aefdc05102e8fabfd8b58b
Ikuzot <110021129+Ikuzot@users.noreply.github.com>
2023-01-07 11:40:50 +0700

    Delete cfg.php

--- Claimbit/cfg.php
+++
@@ -1,8 +0,0 @@
-<?php
-$proxy_ip = '185.199.228.220';
-$proxy_port = '7300';
-$Username = 'Remunan88';
-$Password = 'Remunan88';
-$cok = "PHPSESSID=fie3qj5npa3ajucv5en3uuudpe; RefSource=https://r.search.yahoo.com/_ylt=AwrIQ2cTT7ZjTjMA3ztUACQ5;_ylu=Y29sbwMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1672920979/RO=10/RU=https%3a%2f%2fclaimbits.net%2f/RK=2/RS=80KBPZujun2CeKoqx9DT0tlb4SA-; _gid=GA1.2.721079494.1672892196; clever-last-tracker-53577=1; clever-counter-53577=0-1; bitmedia_fid=eyJmaWQiOiJiZmE3YjY2M2U2YzQzMThkMzViMTcyMjAyZTZiOWFkZCIsImZpZG5vdWEiOiJjODE1ZGNiMDhjZDBmYmViYmUwMDIxMTkxZTkyNGU4OCJ9; AccExist=242183; SesHashKey=zsusoymuwwxqfbu6; SesToken=ses_id=242183&ses_key=zsusoymuwwxqfbu6; cf_clearance=paiChdJhHKNbRoD3mtqJ81D52H0cNwVytVhn1ZIIcyw-1672892371-0-160; _ga_J2L7YD89W2=GS1.1.1672892191.1.1.1672892502.0.0.0; _ga=GA1.2.2063216898.1672892192";
-$us = "Mozilla/5.0 (Linux; Android 11; Pixel xiaomi) AppleWebKit/423.35 (KHTML, like Gecko) Chrome/101.0.348.131 Mobile Safari/421.76";
-
